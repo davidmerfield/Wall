e951edce6f3d8b534706c94f78bab8ae73ac4379
David Merfield <dmerfield@gmail.com>
2019-11-09 19:30:14 -0500

    Initial commit

--- app.js
+++ app.js
@@ -1,71 +1,93 @@
 var APP_KEY = "zl0msi8kqsftxc5";
+var editor;
+var dbx;
+var workingDirectory;
+var workingPath;
 
-var markdownEditor = document.querySelector(".markdown");
-var editor = new MediumEditor('.editable',
-      { placeholder: false,
-        toolbar: {
-          buttons: ['bold', 'italic', 'underline', 'anchor', 'h1', 'h2', 'quote', 'orderedlist', 'unorderedlist']
-        },
-        extensions: {
-            markdown: new MeMarkdown(function (md) {
-              markdownEditor.textContent = md;
-    })}
+if (isAuthenticated()) {
+  showSections("authed");
+  hideSections("unauthed");
+
+  dbx = new Dropbox.Dropbox({
+    accessToken: getAccessToken(),
+    fetch: fetch
   });
 
-function getAccessToken() {
-  if (getAccessTokenFromLocalStorage()) {
-    return getAccessTokenFromLocalStorage();
+  window.onhashchange = router;
+  router();
+} else {
+  showSections("unauthed");
+  hideSections("authed");
+
+  // Set the login anchors href using dbx.getAuthenticationUrl()
+  // clientID === APP_KEY per
+  // https://www.dropboxforum.com/t5/API-Support-Feedback/Javascript-SDK-CLIENT-ID/td-p/217323
+  dbx = new Dropbox.Dropbox({ clientId: APP_KEY, fetch: fetch });
+  document.getElementById("authlink").href = dbx.getAuthenticationUrl(
+    window.location
+  );
+}
+
+function router() {
+  var queryString = parseQueryString(window.location.hash);
+
+  if (!workingDirectory) {
+    renderSelectDirectory();
+  } else if (queryString.file) {
+    showSections("file");
+    hideSections("folder");
+
+    dbx
+      .filesDownload({
+        path: queryString.file || ""
+      })
+      .then(function(response) {
+        renderFile(response);
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  } else if (queryString.folder) {
+    showSections("folder");
+    hideSections("file");
+    dbx
+      .filesListFolder({
+        path: queryString.folder || ""
+      })
+      .then(function(response) {
+        renderItems(response.entries);
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
   }
-
-  if (getAccessTokenFromUrl()) {
-    localStorage.setItem("access_token", getAccessTokenFromUrl());
-    return (window.location = window.location.href.split("#")[0]);
-  }
-
-  return null;
-}
-
-function logOut() {
-  localStorage.setItem("access_token", "");
-  window.location = window.location;
-}
-
-// Parses the url and gets the access token if it is in the urls hash
-function getAccessTokenFromUrl() {
-  return parseQueryString(window.location.hash).access_token;
-}
-
-function getAccessTokenFromLocalStorage() {
-  return localStorage.getItem("access_token");
-}
-
-// If the user was just redirected from authenticating, the urls hash will
-// contain the access token.
-function isAuthenticated() {
-  return !!getAccessToken();
 }
 
 // Render a file to #file
 function renderFile(file) {
+  if (workingDirectory) {
+    showSections("directory-selected");
+    hideSections("select-directory");
+  } else {
+    showSections("select-directory");
+    hideSections("directory-selected");
+  }
   var fileContainer = document.getElementById("file-contents");
   fileContainer.style.display = "block";
-  fileContainer.innerHTML = '';
-  if(file){
-    file.fileBlob.text().then(function(text){
+  fileContainer.innerHTML = "";
+  if (file) {
+    file.fileBlob.text().then(function(text) {
       fileContainer.innerHTML = marked(text);
     });
   }
 
   fileContainer.focus();
-  
-  hidePageSection('authed');
-  hidePageSection('pre-auth');
 }
 
 // Render a list of items to #files
 function renderItems(items) {
   var filesContainer = document.getElementById("files");
-  filesContainer.innerHTML = '';
+  filesContainer.innerHTML = "";
   items.forEach(function(item) {
     var li = document.createElement("li");
     if (item[".tag"] === "folder") {
@@ -87,70 +109,47 @@ function renderItems(items) {
     filesContainer.appendChild(li);
   });
 }
+
+function getAccessToken() {
+  if (localStorage.getItem("access_token")) {
+    return localStorage.getItem("access_token");
+  }
+
+  // Parses the url and gets the access token if it is in the urls hash
+  if (parseQueryString(window.location.hash).access_token) {
+    localStorage.setItem(
+      "access_token",
+      parseQueryString(window.location.hash).access_token
+    );
+    return (window.location = window.location.href.split("#")[0]);
+  }
+
+  return null;
+}
+
+function logOut() {
+  localStorage.setItem("access_token", "");
+  window.location = window.location;
+}
+
+// If the user was just redirected from authenticating, the urls hash will
+// contain the access token.
+function isAuthenticated() {
+  return !!getAccessToken();
+}
+
 // This example keeps both the authenticate and non-authenticated setions
 // in the DOM and uses this function to show/hide the correct section.
-function showPageSection(elementId) {
-  document.getElementById(elementId).style.display = "block";
+function showSections(className) {
+  document.querySelectorAll("." + className).forEach(function(el) {
+    el.style.display = "block";
+  });
 }
 
-function hidePageSection(elementId) {
-  document.getElementById(elementId).style.display = "none";
-}
-
-function publishToDropbox(){
-  if (isAuthenticated()) {
-    showPageSection("authed");
-
-    // Create an instance of Dropbox with the access token and use it to
-    // fetch and render the files in the users root directory.
-    var dbx = new Dropbox.Dropbox({ accessToken: getAccessToken(), fetch: fetch });
-
-    window.onhashchange = router;
-
-    router();
-
-    function router () {
-
-    // We are routing here!
-    if (parseQueryString(window.location.hash).file) {
-      showPageSection("file");
-          hidePageSection("folder");
-
-      dbx
-        .filesDownload({
-          path: parseQueryString(window.location.hash).file || ""
-        })
-        .then(function(response) {
-          renderFile(response);
-        })
-        .catch(function(error) {
-          console.error(error);
-        });
-    } else {
-      showPageSection("folder");
-      hidePageSection("file");
-      dbx
-        .filesListFolder({
-          path: parseQueryString(window.location.hash).folder || ""
-        })
-        .then(function(response) {
-          renderItems(response.entries);
-        })
-        .catch(function(error) {
-          console.error(error);
-        });
-    }
-    }
-
-  } else {
-    showPageSection("pre-auth");
-    // Set the login anchors href using dbx.getAuthenticationUrl()
-    // clientID === APP_KEY per
-    // https://www.dropboxforum.com/t5/API-Support-Feedback/Javascript-SDK-CLIENT-ID/td-p/217323
-    var dbx = new Dropbox.Dropbox({ clientId: APP_KEY, fetch: fetch });
-    var authUrl = dbx.getAuthenticationUrl(window.location);
-    document.getElementById("authlink").href = authUrl;
-  }
+function hideSections(className) {
+  document.querySelectorAll("." + className).forEach(function(el) {
+    el.style.display = "none";
+  });
 }
 
 // Used to extract a user's access token from a URL hash
@@ -199,51 +198,52 @@ var autosaveTimeout = false;
 function saveContent() {
   clearTimeout(autosaveTimeout);
   document.getElementById("post-status").innerHTML = "Saving..";
-  var postContent = document.getElementById('markdown-content').value;
-  document.getElementById("post-wc").innerHTML = "Word Count: " + countWords(postContent);
+  var postContent = document.getElementById("markdown-content").value;
+  document.getElementById("post-wc").innerHTML =
+    "Word Count: " + countWords(postContent);
 
   autosaveTimeout = setTimeout(autoSave, 1000);
 }
 
-function countWords(str){
-  str = str.replace(/(^\s*)|(\s*$)/gi,"") // handle start and end whitespaces
-        .replace(/[ ]{2,}/gi," ") // merge multiple spaces to 1
-        .replace(/\n /,"\n"); // handle newlines
-  return str.split(' ').filter(function(s){return s != "";}).length;
+function countWords(str) {
+  str = str
+    .replace(/(^\s*)|(\s*$)/gi, "") // handle start and end whitespaces
+    .replace(/[ ]{2,}/gi, " ") // merge multiple spaces to 1
+    .replace(/\n /, "\n"); // handle newlines
+  return str.split(" ").filter(function(s) {
+    return s != "";
+  }).length;
 }
 
 function autoSave() {
   autosaveTimeout = false;
   var postData = {
-    body: editor.getContent(),
-  }
-  localforage.setItem('draftpost', postData).then(function(){
+    body: editor.getContent()
+  };
+  localforage.setItem("draftpost", postData).then(function() {
     document.getElementById("post-status").innerHTML = "Saved.";
   });
 }
 
-editor.on(document.getElementById('file-contents'), 'input', function(){
-  saveContent();
-});
-
 // Restore draft posts from local browser storage
-localforage.getItem('draftpost', function(err,val){
-  if(val && val.body) {
+localforage.getItem("draftpost", function(err, val) {
+  if (val && val.body) {
     var fileContainer = document.getElementById("file-contents");
     fileContainer.innerHTML = val.body;
-    document.getElementById("post-status").innerHTML = "Opened last saved draft..";
+    document.getElementById("post-status").innerHTML =
+      "Opened last saved draft..";
     fileContainer.focus();
-  } 
+  }
 });
 
 // Export the content in markdown and save to local
 function saveLocally() {
-  var textToWrite = document.getElementById('markdown-content').value; 
+  var textToWrite = document.getElementById("markdown-content").value;
   // console.log(editor.getContent());
   console.log(textToWrite);
   textToWrite = textToWrite.replace(/\n/g, "\r\n");
-  var textFileAsBlob = new Blob([ textToWrite ], { type: 'text/plain' });
-  
+  var textFileAsBlob = new Blob([textToWrite], { type: "text/plain" });
+
   var textToSaveAsURL = window.URL.createObjectURL(textFileAsBlob);
   var fileNameToSaveAs = "test-post.md";
 
@@ -254,17 +254,43 @@ function saveLocally() {
   downloadLink.style.display = "none";
   document.body.appendChild(downloadLink);
 
-  downloadLink.click();                
+  downloadLink.click();
 }
 
-// Reset editor to new - remove local browser draft 
+// Reset editor to new - remove local browser draft
 function resetEditor() {
-  hidePageSection('pre-auth');
-  hidePageSection('authed');
-  hidePageSection('folder');
-  editor.setContent('');
-  localforage.setItem('draftpost', {})
+  hideSections("pre-auth");
+  hideSections("authed");
+  hideSections("folder");
+  editor.setContent("");
+  localforage.setItem("draftpost", {});
   document.getElementById("file-contents").focus();
 
   window.location.replace("/");
 }
+
+editor = new MediumEditor(".editable", {
+  placeholder: false,
+  toolbar: {
+    buttons: [
+      "bold",
+      "italic",
+      "underline",
+      "anchor",
+      "h1",
+      "h2",
+      "quote",
+      "orderedlist",
+      "unorderedlist"
+    ]
+  },
+  extensions: {
+    markdown: new MeMarkdown(function(md) {
+      document.querySelector(".markdown").textContent = md;
+    })
+  }
+});
+
+editor.on(document.getElementById("file-contents"), "input", function() {
+  saveContent();
+});
